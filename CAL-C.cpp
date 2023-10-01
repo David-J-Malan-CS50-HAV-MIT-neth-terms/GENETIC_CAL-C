5d51c38bf95c0a289442208acb8ce9b6664db868
CELESTIAL SAPIEAN <109981243+David-J-Malan-CS50-HAV-MIT-neth-terms@users.noreply.github.com>
2023-10-01 22:41:55 +0530

    Update CAL-C.cpp
    Brother you should actually present your documentation in the best. I have made some changes on your Parentheses which were in a single line and then made them move to the next line.
    This not only shows your documentation skills but also how well you perform while presenting it to someone who can understand it in easy. 
    Thank me later.
    - MahaRaja Out -

--- CAL-C.cpp
+++ CAL-C.cpp
@@ -13,28 +13,34 @@ int main()
     cout << "**********WELCOME TO GENETIC CAL-C**********" << endl;
     cout << "START CALCULATING \n\n";
 
-    while (x == 1) {
+    while (x == 1)
+    {
 
         cout << "\nEnter Your Calculation" << endl;
         cin >> a >> op >> b;
 
-        if (op == '+') {
+        if (op == '+')
+        {
             ans = a + b;
             cout << "The Sum is " << ans << endl;
         }
-        if (op == '-') {
+        if (op == '-')
+        {
             ans = a - b;
             cout << "The Difference is " << ans << endl;
         }
-        if (op == '*') {
+        if (op == '*')
+        {
             ans = a * b;
             cout << "The Product is " << ans << endl;
         }
-        if (op == '/') {
+        if (op == '/')
+        {
             ans = a / b;
             cout << "The Quotient is " << ans << endl;
         }
-        if (op == '%') {
+        if (op == '%')
+        {
             ans = a * b / 100;
             cout << "The Answer is " << ans << endl;
         }
